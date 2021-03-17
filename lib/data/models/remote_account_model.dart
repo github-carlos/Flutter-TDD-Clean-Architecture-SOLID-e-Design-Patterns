1109974701472c97041382ce77e2e8f9f7a4470f
Carlos E <carloseduardo057@gmail.com>
2021-03-17 14:11:47 -0300

    ensure RemoteAuthentication throws invalidData when 200 with incorrect fields

--- lib/data/models/remote_account_model.dart
+++ lib/data/models/remote_account_model.dart
@@ -1,9 +1,16 @@
 import '../../domain/entities/entities.dart';
 
+import '../http/http.dart';
+
 class RemoteAccountModel {
   final String accessToken;
   RemoteAccountModel(this.accessToken);
-  factory RemoteAccountModel.fromJson(Map json) => RemoteAccountModel(json['accessToken']);
+  factory RemoteAccountModel.fromJson(Map json) {
+    if (!json.containsKey('accessToken')) {
+      throw HttpError.invalidData;
+    }
+    return RemoteAccountModel(json['accessToken']);
+  }
 
   Account toEntity() => Account(this.accessToken);
 }
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -99,4 +99,18 @@ void main() {
 
     expect(account.token, accessToken);
   });
+
+  test(
+      'Should throw UnexpectedError if HttpClient returns 200 with invalid data',
+      () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenAnswer((_) async => {'invalidField': 'invalidValue'});
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
